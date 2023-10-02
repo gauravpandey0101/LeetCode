b92dfa69071da4707ee1ac03abc20319f256d993
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2023-10-02 23:27:59 +0530

    Update Missing Number.java

--- Missing Number.java
+++ Missing Number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
+        int n = nums.length; 
        int totalSum = n*(n+1)/2; 
         int sum = 0;
         for(int i =0;i<n;i++){ 
