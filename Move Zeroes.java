27db8848a201942ba4e4489f60b7dd92e949156f
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2023-10-11 23:43:52 +0530

    Update Move Zeroes.java

--- Move Zeroes.java
+++ Move Zeroes.java
@@ -2,7 +2,7 @@ class Solution {
     public void moveZeroes(int[] nums) { 
     
         int left = 0;   
- 
+  
         for(int right = 0; right < nums.length; right++) {
  
             if(nums[right] != 0) {
