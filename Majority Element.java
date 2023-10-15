d9e06b413c257e8c982d66b5aad2691fb9283dd1
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2023-10-15 23:34:55 +0530

    Update Majority Element.java

--- Majority Element.java
+++ Majority Element.java
@@ -1,7 +1,7 @@
 class Solution {  
 public int majorityElement(int[] nums) {     
 int ansIndex=0; 
-int count=1;
+int count=1; 
 
     for(int i=1;i<nums.length;i++){ 
         if(nums[i]==nums[ansIndex]){
