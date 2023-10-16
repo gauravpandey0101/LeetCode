2fb12820925c75efcce74332f4a9a6067b654bc0
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2023-10-16 22:04:29 +0530

    Update Path Sum.java

--- Path Sum.java
+++ Path Sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-         if (root == null) 
+         if (root == null)  
       return false;    
     if (root.val == targetSum && root.left == null && root.right == null)
       return true;
