8bd0e09683601478f669ea7615e8d4f2cd79a11b
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2023-10-19 20:46:22 +0530

    Update Next Greater Element I.java

--- Next Greater Element I.java
+++ Next Greater Element I.java
@@ -1,7 +1,7 @@
 	int[] resultArray = new int[nums1.length];
 		int flag=0, max;         
 		for(int i=0;i<nums1.length;i++){ 
-			max=0; 
+			max=0;  
 			flag=0;   
 			for(int j=0;j<nums2.length;j++){
 				if(nums2[j]==nums1[i]) 
