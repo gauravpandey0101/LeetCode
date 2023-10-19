114d25ab5e1b02634620d3672767f491aa97953a
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2023-10-19 20:46:42 +0530

    Update Next Greater Element I.java

--- Next Greater Element I.java
+++ Next Greater Element I.java
@@ -2,7 +2,7 @@
 		int flag=0, max;         
 		for(int i=0;i<nums1.length;i++){ 
 			max=0;  
-			flag=0;    
+			flag=0;     
 			for(int j=0;j<nums2.length;j++){
 				if(nums2[j]==nums1[i]) 
 					flag=1;
