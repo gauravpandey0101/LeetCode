638ce1128e3ba84e76da9b37ea37ba38170e830f
Gaurav Pandey <107850615+gauravpandey0101@users.noreply.github.com>
2023-10-19 20:45:21 +0530

    Create Next Greater Element I.java

---
+++ Next Greater Element I.java
@@ -0,0 +1,16 @@
+	int[] resultArray = new int[nums1.length];
+		int flag=0, max;        
+		for(int i=0;i<nums1.length;i++){
+			max=0;
+			flag=0;
+			for(int j=0;j<nums2.length;j++){
+				if(nums2[j]==nums1[i])
+					flag=1;
+				else if(flag==1 && nums2[j]>nums1[i]){
+					max=nums2[j];
+					break;
+				}
+			}
+			resultArray[i]= max!=0 ? max : -1 ;
+		}
+		return resultArray;
